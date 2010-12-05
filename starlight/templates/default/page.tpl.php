81a5f87a88bd8d081b69a4902fa677adfd0539aa
colummcgaley <colum@gmx.com>
2010-12-05 08:13:45 -0500

    Fixed default themes

---
+++ starlight/templates/default/page.tpl.php
@@ -0,0 +1,17 @@
+<?php
+	include 'header.tpl.php'; # Makesure we have the header included
+?>
+	<div class="post">
+		<h2><a href="?f=post/<?php echo $this->meta['id']; ?>" rel="bookmark"><?php echo $this->meta['title']; ?></a></h2>
+	    <small>20th June, 2008 <!-- by Theme Admin --></small>
+		<div class="entry">
+			<?php echo $this->body; ?>
+		</div>
+		<div>
+			Comments go here
+		</div>
+	</div>
+
+<?php
+	include 'footer.tpl.php'; # Makesure we have the header included
+?>
