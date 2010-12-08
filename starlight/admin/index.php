80011bb660112471996231a10bd2a869a8dce3df
colummcgaley <colum@gmx.com>
2010-12-08 18:18:32 -0500

    Fixed undefined index warnings and some other general fixes

--- starlight/admin/index.php
+++ starlight/admin/index.php
@@ -2,7 +2,7 @@
 	require '../../config.php';
 	require 'adminFunctions.php';
 	
-	if($_GET['do'] == 'process') {
+	if(isset($_GET['do']) and $_GET['do'] == 'process') {
 		adminDoProcess();
 	}
 ?>
@@ -18,19 +18,19 @@
 	</head>
 	<body>
 
-		<h1 id="head">Steal My Admin Template</h1>
+		<h1 id="head">Starlight .0.0.1</h1>
 		
 		<ul id="navigation">
 			<li><span class="active">Overview</span></li>
-			<li><a href="?do=new-page">New Post</a></li>
-			<li><a href="#">New Page</a></li>
-			<li><a href="#">Manage</a></li>
-			<li><a href="#">Settings</a></li>
+			<li><a href="?do=new-post">New Post</a></li>
+			<li><a href="?do=new-page">New Page</a></li>
+			<li><a href="?do=manage">Manage</a></li>
+			<li><a href="?do=settings">Settings</a></li>
 		</ul>
 		
 			<div id="content" class="container_16 clearfix">
 			<?php
-				if($_GET['do'] == 'new-post'):
+				if(isset($_GET['do']) and $_GET['do'] == 'new-post'):
 			?>
 				<div id="content" class="container_16 clearfix">
 					<form method="post" action="?do=process">
@@ -74,7 +74,7 @@
 					
 				</div>
 			<?php
-				endif;
+				else:
 			?>
 				<div class="grid_5">
 					<div class="box">
@@ -85,39 +85,6 @@
 						<p><strong>Last Signed In : </strong> Wed 11 Nov, 7:31<br /><strong>IP Address : </strong> 192.168.1.101</p>
 					</div>
 					<div class="box">
-						<h2>Files</h2>
-						<div class="utils">
-							<a href="#">View More</a>
-						</div>
-						<table>
-							<tbody>
-								<tr>
-									<td>Newton 2</td>
-									<td>8/10</td>
-								</tr>
-								<tr>
-									<td>Wicked Twister</td>
-									<td>9/10</td>
-								</tr>
-								<tr>
-									<td>Forester</td>
-									<td>9.12/10</td>
-								</tr>
-								<tr>
-									<td>Sabertooth</td>
-									<td>8.9/10</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-					<div class="box">
-						<h2>Messages</h2>
-						<div class="utils">
-							<a href="#">Inbox</a>
-						</div>
-						<p class="center">Have have <a href="#">10</a> unread messages.</p>
-					</div>
-					<div class="box">
 						<h2>CMS Updates</h2>
 						<div class="utils">
 							<a href="#">Check</a>
@@ -126,32 +93,6 @@
 					</div>
 				</div>
 				<div class="grid_6">
-					<div class="box">
-						<h2>At a Glance</h2>
-						<div class="utils">
-							<a href="#">View More</a>
-						</div>
-						<table>
-							<tbody>
-								<tr>
-									<td>1 Post</td>
-									<td>2 Comments</td>
-								</tr>
-								<tr>
-									<td>1 Page</td>
-									<td>2 Approved</td>
-								</tr>
-								<tr>
-									<td>1 Categories</td>
-									<td>0 Pending</td>
-								</tr>
-								<tr>
-									<td>0 Tags</td>
-									<td>0 Spam</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
 					<div class="box">
 						<h2>Quick Post</h2>
 						<div class="utils">
@@ -195,87 +136,9 @@
 							</tbody>
 						</table>
 					</div>
-					<div class="box">
-						<h2>Schedule</h2>
-						<div class="utils">
-							<a href="#">View More</a>
-						</div>
-						<table class="date">
-							<caption><a href="#">Prev</a> November 2009 <a href="#">Next</a> </caption>
-							<thead>
-								<tr>
-									<th>Mon</th>
-									<th>Tue</th>
-									<th>Wed</th>
-									<th>Thu</th>
-									<th>Fri</th>
-									<th>Sat</th>
-									<th>Sun</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td><a href="#">1</a></td>
-								</tr>
-								<tr>
-									<td><a href="#">2</a></td>
-									<td><a href="#">3</a></td>
-									<td><a href="#">4</a></td>
-									<td><a href="#">5</a></td>
-									<td><a href="#">6</a></td>
-									<td><a href="#">7</a></td>
-									<td><a href="#">8</a></td>
-								</tr>
-								<tr>
-									<td><a href="#">9</a></td>
-									<td><a href="#">10</a></td>
-									<td><a href="#" class="active">11</a></td>
-									<td><a href="#">12</a></td>
-									<td><a href="#">13</a></td>
-									<td><a href="#">14</a></td>
-									<td><a href="#">15</a></td>
-								</tr>
-								<tr>
-									<td><a href="#">16</a></td>
-									<td><a href="#">17</a></td>
-									<td><a href="#">18</a></td>
-									<td><a href="#">19</a></td>
-									<td><a href="#">20</a></td>
-									<td><a href="#">21</a></td>
-									<td><a href="#">22</a></td>
-								</tr>
-								<tr>
-									<td><a href="#">23</a></td>
-									<td><a href="#">24</a></td>
-									<td><a href="#">25</a></td>
-									<td><a href="#">26</a></td>
-									<td><a href="#">27</a></td>
-									<td><a href="#">28</a></td>
-									<td><a href="#">29</a></td>
-								</tr>
-								<tr>
-									<td><a href="#">30</a></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-									<td></td>
-								</tr>
-							</tbody>
-						</table>
-						<ol>
-							<li>Draft contract template.</li>
-							<li>Draft invoice template.</li>
-							<li>Draft business cards.</li>
-						</ol>
-					</div>
+				<?php
+					endif;
+				?>
 				</div>
 			</div>
 		<div id="foot">
