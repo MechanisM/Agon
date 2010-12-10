988e49bacd1e4f07a85de7002f3d508e4b37ed25
colummcgaley <colum@gmx.com>
2010-12-09 21:42:12 -0500

    Fixed default theme

--- starlight/templates/default/posts.tpl.php
+++ starlight/templates/default/posts.tpl.php
@@ -6,23 +6,23 @@
 	if($this->nav[0])
 		echo "Go next";
 ?>
-
-    <?php for ($i = $this->posts[0]; $i < $this->posts[1]; $i++): ?>
-		<?php 
-			if($this->posts[2][$i][0]):
-			$info = json_decode($this->posts[2][$i][1]);
-		?>
-		<div class="post" id="post-4">
-			<h2><a href="?f=post/<?php echo $this->posts[2][$i][0]; ?>" rel="bookmark"><?php echo $info->title; ?></a></h2>
-		    <small>20th June, 2008 <!-- by Theme Admin --></small>
-			<div class="entry">
-				<?php echo $this->posts[2][$i][2]; ?>
+<?php for ($i = $this->posts[1]; $i >= $this->posts[0]; $i--): ?>
+	<?php if(isset($this->posts[2][$i])): 
+			$info = (object) array('id' => _i($this->posts[2][$i],0),'title' => _i($this->posts[2][$i],1), 'date' => _i($this->posts[2][$i],2), 'body' => _i($this->posts[2][$i],3));
+	?>
+			<div class="post" id="post-4">
+				<h2><a href="?f=post/<?php echo gS($info->id); ?>" rel="bookmark"><?php echo $info->title; ?></a></h2>
+			    <small><?php echo $info->date; ?></small>
+				<div class="entry">
+					<?php echo $this->posts[2][$i][2]; ?>
+				</div>
+				<p class="postmetadata">
+					Posted in <a href="http://wp-themes.com/?cat=6" title="View all posts in First Child Category" rel="category">First Child Category</a> | <span>Comments Off</span>
+				</p>
 			</div>
-			<p class="postmetadata">
-				Posted in <a href="http://wp-themes.com/?cat=6" title="View all posts in First Child Category" rel="category">First Child Category</a> | <span>Comments Off</span>
-			</p>
-		</div>
-    <?php endif; endfor; ?>
+	<?php endif; ?>
+<?php endfor; ?>
+
 <?php
 	include 'footer.tpl.php'; # Makesure we have the header included
 ?>
