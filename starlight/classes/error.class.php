cdf7ec7cf3bb54f40847c94fbb35979d23c1fb18
colummcgaley <colum@gmx.com>
2010-12-10 20:46:31 -0500

    Removed unused files

--- starlight/classes/error.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class error {
-		private $message = "
-Warning. Starlight has encountered a critical error and has had to abort the current operation. A message has been sent to the developer, in order to help fix this. 
-
-Status Code
-		";
-	}
-?>
