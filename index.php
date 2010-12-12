81b742cb6d56bd75e089e6d1b490ccc5a8cc1f5f
colummcgaley <c.mcgaley@gmail.com>
2010-12-11 20:14:23 -0500

    Updated install url

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
 	//index.php file for Starlight. Yo
 	if(!file_exists("config.php"))
-		die("Starlight has not been installed or the config file is corrupt. Please go <a href='starlight/install/install.php'>Here to start the installer</a>");
+		die("Starlight has not been installed or the config file is corrupt. Please go <a href='starlight/'>Here to start the installer</a>");
 	
 	require("config.php");
 	
