e05427e77569880892f44b035bf492edf78dd100
Colum McGaley <c.mcgaley@gmail.com>
2010-12-24 11:24:07 -0500

    Added login template

---
+++ starlight/admin/login.tpl.php
@@ -0,0 +1,17 @@
+		<form action="?" method="post">
+			<fieldset>
+				<table class="nostyle">
+					<tr>
+						<td style="width:70px;">Username:</td>
+						<td><input type="text" size="40" name="u" /></td>
+					</tr>
+					<tr>
+						<td style="width:70px;">Password:</td>
+						<td><input type="password" size="40" name="p" /></td>
+					</tr>					
+					<tr>
+						<td colspan="2" class="t-right"><input type="submit" class="input-submit" value="Submit" /></td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
