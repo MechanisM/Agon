97070b3114b3b6eb3e72a6051e026173ed6abf56
Colum McGaley <c.mcgaley@gmail.com>
2010-12-24 08:56:28 -0500

    Added corrent form for creatng a new post

--- starlight/admin/write.realm.php
+++ starlight/admin/write.realm.php
@@ -1,31 +1,26 @@
 <?php 
-	include 'data/header.tpl.php';
+	require 'theme/header.tpl.php';
 ?>
-	<div class="submenu"><a href="#">Sub Menu1</a> / <a href="#">Sub Menu2</a> / <a href="#">Sub Menu3</a></div>
-	<div class="spacer"></div>
-	<div class="content">
-	<h1>Write</h1>
-	
-	<p>As simple as it gets:zero table, one-column, two (three?)-tone color scheme. XHTML 1.0 Strict and CSS valid. Tested in FireFox, 
-	Opera, and Internet Explorer. Please retain the "Template design by MLP Design" 	tag and link in the footer when you use this template. Thanks.</p>
-	<blockquote><p>This is how a blockquote would look. This is an example of a blockquote. This is a blockquote. This is how a blockquote would look. This is an example of a blockquote. This is a blockquote. This is how a blockquote would look. This is an example of a blockquote. This is a blockquote. </p></blockquote>
-	<p>And now, some space fillers. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce at velit ac neque scelerisque blandit.
-	Aenean malesuada. Suspendisse vestibulum orci. Quisque posuere vehicula justo. Integer sollicitudin ligula vitae tellus. Vestibulum 
-	nibh vel quam molestie hendrerit. Proin posuere magna vitae neque. Maecenas id ante. Etiam nec ante. Morbi ultrices magna et neque.
-	In hac habitasse platea dictumst. Quisque vehicula ultricies neque. Aenean volutpat varius lectus. Morbi tempor justo at ante.</p>
-	</div>
-	<div class="spacer"></div>
-	<div class="content">
-	<h1>Sample Lists</h1>
-	<h2>Here is an example of an unordered list and an ordered list</h2>
-	<ul><li>Unordered List One</li><li>Unordered List Two</li><li>Unordered List Three</li></ul>
-	<ol><li>Ordered List A</li><li>Ordered List B</li><li>Ordered List C</li></ol>
-<p>And still some more space fillers. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce at velit ac neque scelerisque blandit.
-	Aenean malesuada. Suspendisse vestibulum orci. Quisque posuere vehicula justo. Integer sollicitudin ligula vitae tellus. Vestibulum 
-	nibh vel quam molestie hendrerit. Proin posuere magna vitae neque. Maecenas id ante. Etiam nec ante. Morbi ultrices magna et neque.
-	In hac habitasse platea dictumst. Quisque vehicula ultricies neque. Aenean volutpat varius lectus. Morbi tempor justo at ante.</p>	
-	</div>
+		<form action="?" method="post">
+			<fieldset>
+				<legend>New Post</legend>
+				<table class="nostyle">
+					<tr>
+						<td style="width:70px;">Title:</td>
+						<td><input type="text" size="40" name="title" class="input-text" /></td>
+					</tr>
+					<tr>
+						<td class="va-top">Body:</td>
+						<td><textarea cols="100" rows="15" class="input-text" name="body"></textarea></td>
+					</tr>
+					<tr>
+						<td colspan="2" class="t-right"><input type="submit" class="input-submit" value="Submit" /></td>
+					</tr>
+				</table>
+			</fieldset>
+			<input type="hidden" name="realm" value="write" />
+			<input type="hidden" name="function" value="post" />
+		</form>
 <?php 
-	include 'data/footer.tpl.php';
+	require 'theme/footer.tpl.php';
 ?>
-	
