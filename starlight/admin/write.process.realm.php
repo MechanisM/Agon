5958ad2e711c539caf5717a91f6547edd2dcf9e8
Colum McGaley <c.mcgaley@gmail.com>
2010-12-24 08:57:27 -0500

    Added post saving

---
+++ starlight/admin/write.process.realm.php
@@ -0,0 +1,42 @@
+<?php
+	/* 0 ID
+	 * 1 Slug
+	 * 2 Title
+	 * 3 Date
+	 * 4 Author
+	 * 5 Body
+	 * 6 Comments
+	 * 7 Disable Time
+	 * 8 Syntax
+	 * 9 
+	 */
+	if(!isset($_POST['title']) or !isset($_POST['title'])) { # Make sure we have a title
+		die("One of the feilds was left empty");
+	}
+	# TODO generate ID
+	$redis->rpush("slight.post.",$id,$id);	
+	# Generate post slug
+		if(isset($_POST['slug']) and $_POST['slug'] != "") {
+			$slug = strtolower(str_replace(' ', '-', $_POST['slug']));
+		} else {
+			$slug = strtolower(str_replace(' ', '-', $_POST['title']));
+		}
+		$o = 2; # We will just add another number to the end of the slug
+		do {
+			$slug .= "-".$o;
+			$o++; # Increase the number
+		} while ($redis->get("slight.slug.".$slug));
+		
+	$redis->set("slight.slug.".$slug, $id);	# Set the slug, so we can access it
+	$redis->set("slight.slug.".$id, $slug); # We can access the post via id or slug
+	
+	$redis->rpush("slight.post.",$id, $slug);	
+	$redis->rpush("slight.post.",$id, strip_tags(trim($_POST['title']))); # Set the title in the 3rd place
+	$redis->rpush("slight.post.",$id, time()); # Time
+	$redis->rpush("slight.post.",$id, "Admin"); # Set the title in the 3rd place
+	$redis->rpush("slight.post.",$id, $_POST['body']); # Set the title in the 3rd place
+
+	$redis->rpush("slight.post.",$id, "true"); # Comments enabled?
+	$redis->rpush("slight.post.",$id, "6"); # Time (in weeks) to disable comments after
+	$redis->rpush("slight.post.",$id, "textile"); # Markup language
+	
