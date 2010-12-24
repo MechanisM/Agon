05da81242a75379bd11bf150c9c9b60e64b6ad68
Colum McGaley <c.mcgaley@gmail.com>
2010-12-24 11:19:59 -0500

    Moved constants from kickstarter to config, and added password field for predis

--- config.php
+++ config.php
@@ -1,7 +1,16 @@
 <?php
+
+	define("s_version", "0.0.4");
+	define("s_release", false); # Set to false for develoment
+	define("s_admin", true); # Set to false to disable the admin
+	define("_PATH", dirname(__FILE__).'/starlight');
+	
+	
 	$single_server = array(
     	'host'     => '127.0.0.1', 
     	'port'     => 6379, 
-    	'database' => 0
+    	'database' => 0,
+		#'password' => ''
 	);
+	
 ?>
