e6f0839d9013b0bafec7996d0dde5bb3f15f3412
Colum McGaley <c.mcgaley@gmail.com>
2010-12-24 11:28:25 -0500

    Added session start

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-
+	session_start();
+	
 	define("s_version", "0.0.4");
 	define("s_release", false); # Set to false for develoment
 	define("s_admin", true); # Set to false to disable the admin
 	define("_PATH", dirname(__FILE__).'/starlight');
 	
-	
+	# Redis Config
 	$single_server = array(
     	'host'     => '127.0.0.1', 
     	'port'     => 6379, 
