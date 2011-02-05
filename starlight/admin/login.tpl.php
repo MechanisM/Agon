0262678bf13cf0dac3fcbbd71b416d0f7ba389e7
Colum McGaley <c.mcgaley@gmail.com>
2011-02-04 21:02:26 -0500

    Updated admin theme

--- starlight/admin/login.tpl.php
+++ starlight/admin/login.tpl.php
@@ -1,17 +1,28 @@
-		<form action="?" method="post">
-			<fieldset>
-				<table class="nostyle">
-					<tr>
-						<td style="width:70px;">Username:</td>
-						<td><input type="text" size="40" name="u" /></td>
-					</tr>
-					<tr>
-						<td style="width:70px;">Password:</td>
-						<td><input type="password" size="40" name="p" /></td>
-					</tr>					
-					<tr>
-						<td colspan="2" class="t-right"><input type="submit" class="input-submit" value="Submit" /></td>
-					</tr>
-				</table>
-			</fieldset>
-		</form>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>Starlight :: Login</title>
+		<link rel="stylesheet" type="text/css" href="admin/theme/css/style.css" />
+	</head>
+	<body>
+		<div id='main'>
+			<div id='all'>
+				<div><form method='post' action=''>
+					<h1>Starlight</h1>
+					<br />
+					<p><strong>Login:</strong> (4-12 chars) 
+						<input name='uid' type='text' maxlength='12' /></p>
+					<p><strong>Password:</strong> (4-12 chars) 
+						<input name='pwd' type='password' maxlength='12' /></p>
+				
+					<p><input name='submitLogin' type='submit' value='Login' class='login-button' /></p>
+					<p>&nbsp;</p>
+				</form></div>
+			</div>	
+		</div>
+
+	</body>
+</html>
