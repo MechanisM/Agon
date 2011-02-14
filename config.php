33ae45d03bde657919d245b5b15f35329417d9fb
Colum McGaley <c.mcgaley@gmail.com>
2011-02-14 15:44:30 -0500

    Updated path variable

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 	define("s_version", "0.0.4");
 	define("s_release", false); # Set to false for develoment
 	define("s_admin", true); # Set to false to disable the admin
-	define("_PATH", dirname(__FILE__).'/starlight');
+	define("_PATH_", dirname(__FILE__).'/starlight');
 	
 	# Redis Config
 	$single_server = array(
