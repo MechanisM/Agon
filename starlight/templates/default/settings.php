d364c7e87ee41e8f47d917ff0583bf1ac8184def
Colum McGaley <c.mcgaley@gmail.com>
2011-02-13 19:57:04 -0500

    Added settings file for theme

---
+++ starlight/templates/default/settings.php
@@ -0,0 +1,23 @@
+<?php
+    
+    define('THEME_NAME',        'Default');
+    define('THEME_VERSION',     '1.0');
+    define('THEME_AUTHOR',      'Colum McGaley');
+    define('THEME_URL',         'projects.archangel.io/starlight');
+    define('THEME_DESCRIPTION', 'This is the default theme. Just a framework of what you could do');
+
+    # Installer function
+    function install_theme() {
+        # The theme has access to a hash named slight.theme.THEME_NAME
+    }
+    
+    function display_settings() {
+        
+    }
+    
+    function save_settings() {
+        
+    }
+    
+    
+?>
