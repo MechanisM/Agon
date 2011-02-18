fbf2f303aeaa6fdbc6efdd8f2eaf2c972131a64a
Colum McGaley <c.mcgaley@gmail.com>
2011-02-17 21:46:29 -0500

    Cleaned up both of these files

--- config.php
+++ config.php
@@ -1,17 +1,13 @@
 <?php
-	session_start();
-	
-	define("s_version", "0.0.4");
-	define("s_release", false); # Set to false for develoment
+    define("s_version", "0.3.3");
+	define("s_release", true); # Set to false for develoment
 	define("s_admin", true); # Set to false to disable the admin
 	define("_PATH_", dirname(__FILE__).'/starlight');
 	
-	# Redis Config
 	$single_server = array(
     	'host'     => '127.0.0.1', 
     	'port'     => 6379, 
     	'database' => 0,
 		#'password' => ''
 	);
-	
 ?>
--- index.php
+++ index.php
@@ -15,7 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    //index.php file for Starlight. Yo
+	session_start();
+    
     if(!file_exists("config.php"))
         die("Starlight has not been installed or the config file is corrupt. Please go <a href='starlight/'>Here to start the installer</a>");
 	
