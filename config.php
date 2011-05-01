400d1085c6850d0207c8cd1438f460d9fae7125a
Colum McGaley <c.mcgaley@gmail.com>
2011-05-01 13:51:51 -0400

    Cleaned up version number for pubic display

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-    define("s_version", "dev.agon-0.4.6");
-	define("s_release", true); # Set to false for develoment
-	define("s_admin", true); # Set to false to disable the admin
-	define("_PATH_", dirname(__FILE__).'/agon');
-	
+    define("s_version", "0.4.6a");
+    define("s_release", true); # Set to false for develoment
+    define("s_admin", true); # Set to false to disable the admin
+    define("_PATH_", dirname(__FILE__).'/agon');
+    
 	$single_server = array(
 	    'host'     => '127.0.0.1', 
 	    'port'     => 6379, 
