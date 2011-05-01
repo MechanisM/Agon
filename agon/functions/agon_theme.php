3d8d90f32282931b042c024e7005d562e43e6ff0
Colum McGaley <c.mcgaley@gmail.com>
2011-05-01 10:51:05 -0400

    Added back all the theme functions

--- agon/functions/agon_theme.php
+++ agon/functions/agon_theme.php
@@ -16,5 +16,33 @@
 		$s->readcomments($i);
 	}
 	
-	
+	function meta2obj($arr) {
+	        global $redis;
+	        return (object) array( 'id' => _i($arr,0),
+	                                'slug' => _i($arr,1),
+	                                'title' => _i($arr,2),
+	                                'date' => _i($arr,3),
+	                                'author' => _i($arr,4),
+	                                'body' => truncate(_i($arr,5),((int)$redis->get('slight.config.trim'))),
+	                             );
+	}
+
+	function url($place) {
+		if(_c('slight.config.cleanurl') == 'true')
+			return _c('slight.config.siteurl').'/'.$place;
+	}
+    
+	function truncate($str, $n, $delim='...') {
+		if($n == 0) {
+			return $str;
+		} else {
+			$len = strlen($str);
+			if ($len > $n) {
+				preg_match('/(.{' . $n . '}.*?)\b/', $str, $matches);
+				return rtrim($matches[1]) . $delim;
+			} else {
+				return $str;
+			}
+		}
+	}
 ?>
