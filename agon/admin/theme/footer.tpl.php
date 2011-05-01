8be24f98f5d0ee068630c5d8d8bbd6649b8b5e04
Colum McGaley <c.mcgaley@gmail.com>
2011-05-01 10:12:02 -0400

    Updated footer, made verion dynamic

--- agon/admin/theme/footer.tpl.php
+++ agon/admin/theme/footer.tpl.php
@@ -3,9 +3,19 @@
 				</div>
 				<div id='footer' class='c2'>
 					<div class='col'>&copy; 20011</div>
-					<div class='col right'><a href='http://projects.archangel.io/starlight'>Starlight<small></small> v0.13a</a></div>
+					<div class='col right'><a href='http://redir.archangel.io/starlight?v=<?php echo s_version; ?>'>Starlight<small></small> v<?php echo s_version; ?></a></div>
 					<div class='cl'><!-- --></div>
 				</div>
+			</div>
+			<div id='donate'>
+				If you find this software useful, please donate a little. (You can disable this message in the settings)
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="PXQXGY7CQG6ES">
+					<input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+
 			</div>
 		</body>
 	</html>
