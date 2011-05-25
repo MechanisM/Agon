8d12dd8d0b173265d2c49c5979306a26df4817c5
Colum McGaley <c.mcgaley@gmail.com>
2011-05-24 21:19:26 -0400

    Added en-us language file

---
+++ agon/lang/en-us.php
@@ -0,0 +1,11 @@
+<?php
+
+    $l = array();
+    
+    # Errors
+        $l['error']['database_failure_no_admin_user'] = "Can not find any admin user";
+        
+        $l['error']['empry_field'] = "One of the required forms was not filled in";
+        $l['error']['invalid_email'] = "Invalid Email";
+        $l['error']['invalid_human'] = "Please try the verification code again";
+?>
